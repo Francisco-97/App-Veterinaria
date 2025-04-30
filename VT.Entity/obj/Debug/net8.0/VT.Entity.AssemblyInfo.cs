ac0da8e1fdc442d25a908d8d674095cdf8e68ab1
Mabel Cabrera <mcabrera@simetricaconsulting.com>
2025-04-30 12:21:45 -0400

    Agregue clase persona

--- VT.Entity/obj/Debug/net8.0/VT.Entity.AssemblyInfo.cs
+++ VT.Entity/obj/Debug/net8.0/VT.Entity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VT.Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d344d5a23d211a30a226091e2ca8551181c6c17e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23664d2d97ac8acd982e728e49deba2eb6f9766f")]
 [assembly: System.Reflection.AssemblyProductAttribute("VT.Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VT.Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
